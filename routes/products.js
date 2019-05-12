4e8681cf3b9ef3d05f161d7768110e45c9bd8773
shifali <shifali.gupta183@gmail.com>
2019-05-12 18:18:24 +0530

    get category

--- routes/products.js
+++ routes/products.js
@@ -7,4 +7,10 @@ router.get('/', async(req, res) => {
     res.render('products',{title:'Products', products});
 });
 
+router.get('/:category', async(req, res) => {
+    let category = req.params.category;
+    let products = await Product.find({category});
+    res.render('products',{title:'Products', products});
+});
+
 module.exports = router;
