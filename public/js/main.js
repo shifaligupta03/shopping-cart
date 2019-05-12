62b5315c6e17e692f0ce1dfc2902d46fc5cbc335
shifali <shifali.gupta183@gmail.com>
2019-05-12 21:41:35 +0530

    fancybox

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,8 @@ $(function(){
     $('a.confirmDeletion').on('click', function(){
         if(!confirm('Are you sure you want to delete it?')) return false;
     });
+
+    if($('[data-fancybox]').length){
+        $('[data-fancybox]').fancybox();
+    }
 });
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
 const {Product} = require('../models/product');
+const fs = require('fs-extra');
 
 router.get('/', async(req, res) => {
     let products = await Product.find();
@@ -11,6 +13,15 @@ router.get('/:category', async(req, res) => {
     let category = req.params.category;
     let products = await Product.find({category});
     res.render('products',{title:'Products', products});
+});
+
+router.get('/:category/:product', async(req, res) => {
+    let slug = req.params.product;
+    let product = await Product.findOne({slug});
+    let productImagesPath = config.get('product_images_path');
+    let galleryDir = productImagesPath+'/'+product._id+'/gallery/';
+    let galleryImages = await fs.readdir(galleryDir);
+    res.render('product_detail',{title:product.title, product, galleryImages });
 });
 
 module.exports = router;
